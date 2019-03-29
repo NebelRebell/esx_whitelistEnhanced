b7bc4c634e691899118013d6cec6e81120724eaf
NebelRebell <40825041+NebelRebell@users.noreply.github.com>
2019-03-29 16:58:26 +0100

    Create de.lua
    Translated from EN to DE.

---
+++ locales/de.lua
@@ -0,0 +1,20 @@
+Locales['de'] = {
+	['not_in_whitelist'] 				= 'Dieser Server verwendet eine Whitelist. Weitere Informationen erhälst Du hier: %s',
+	['missing_steam_id'] 				= 'Deine SteamID ist nicht erkennbar! Steam gestartet?',
+	['banned_from_server'] 				= 'Du bist auf diesem Server nicht ohne Grund gebannt!',
+	['waiting_queue_message'] 			= 'Du befindest Dich in der Warteschlange (%i/%i)',
+	['waiting_free_priority_slots'] 	= 'Warten auf einen Prioritätsslot (%i Prioritäts-Slots, geschätzte Wartezeit %im %is)',
+	['log_trying_to_connect'] 			= '%s [%s] versucht, sich zu verbinden.',
+	['log_missing_steam_id'] 			= 'Anwohner %s hat keinen steamID!',
+	['log_banned_from_server'] 			= '%s [%s] ist gebannt : \'%s\'',
+	['log_not_in_whitelist'] 			= '%s [%s] ist nicht auf der Whitelist!',
+	['log_already_in_priority_queue'] 	= '%s [%s] befindet sich bereits in der Prioritätswarteschlange!',
+	['log_added_to_priority_queue'] 	= '%s [%s] wurde der Prioritätswarteschlange hinzugefügt.',
+	['log_removed_from_priority_queue'] = '%s [%s] wurde aus der Prioritätswarteschlange entfernt.',
+	['log_player_connected_as_vip'] 	= '%s eingeloggt als VIP',
+	['log_started_anti_spam'] 			= 'Anti-Spamsystem gestartet für %s',
+	['anti_spam_message'] 				= 'Anti-Spamprotection: Bitte warte %i Sekunden, Du wirst automatisch verbunden.',
+	['log_stopped_anti_spam'] 			= 'Anti-Spamsystem angehalten für %s',
+	['reload_whitelist'] 				= 'Whitelist Refresh',
+	['whitelist_reloaded'] 				= 'Whitelist erfolgreich neu eingeladen!'
+}
